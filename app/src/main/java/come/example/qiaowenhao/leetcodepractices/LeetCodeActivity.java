79b5d1e9f6cd5de1e1a418b545a0906aef44b0a5
qiaowenhao <qiaowenhao@xiaomi.com>
2019-12-01 13:52:39 +0800

    [Feature]:双指针和链表
    Signed-off-by: qiaowenhao <qiaowenhao@xiaomi.com>

--- app/src/main/java/come/example/qiaowenhao/leetcodepractices/LeetCodeActivity.java
+++ app/src/main/java/come/example/qiaowenhao/leetcodepractices/LeetCodeActivity.java
@@ -3,6 +3,7 @@ package come.example.qiaowenhao.leetcodepractices;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -18,6 +19,8 @@ public class LeetCodeActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_leetcode);
+        int[] nums = {2,3,1,2,4,3};
+        Log.d("iniesta", "onCreate: " + minSubArrayLen(7,nums));
     }
 
     // 349. Intersection of Two Arrays
@@ -207,4 +210,318 @@ public class LeetCodeActivity extends AppCompatActivity {
         return length;
     }
 
+    // 16. 3Sum Closest
+    //***
+    public int threeSumClosest(int[] nums, int target) {
+        if (nums == null || nums.length < 3) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int res = nums[0] + nums[1] + nums[nums.length - 1];
+        for (int i = 0; i < nums.length - 2; i++) {
+            int start = i + 1;
+            int end = nums.length - 1;
+            while (start < end) {
+                int tmp = nums[i] + nums[start] + nums[end];
+                if (tmp < target) {
+                    start++;
+                } else if (tmp > target) {
+                    end--;
+                } else {
+                    return tmp;
+                }
+                res = Math.abs(tmp - target) < Math.abs(res - target) ? tmp : res;
+            }
+        }
+        return res;
+    }
+
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
+    // 142. Linked List Cycle II
+    // ***
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCirlce = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCirlce = true;
+                break;
+            }
+        }
+        if (!hasCirlce) {
+            return null;
+        }
+        while (slow != head) {
+            slow = slow.next;
+            head = head.next;
+        }
+        return head;
+    }
+
+    // 86. Partition List
+    // ***　注意是值传递并不是引用传递
+    public ListNode partition(ListNode head, int x) {
+        ListNode lower = new ListNode(-1);
+        ListNode lower1 = lower;
+        ListNode higher = new ListNode(-1);
+        ListNode higher1 = higher;
+        while (head != null) {
+            if (head.val < x) {
+                lower.next = head;
+                lower = lower.next;
+            } else {
+                higher.next = head;
+                higher = higher.next;
+            }
+            head = head.next;
+        }
+        higher.next = null;
+        lower.next = higher1.next;
+        return lower1.next;
+    }
+
+   // 80. Remove Duplicates from Sorted Array II
+    //****
+    public int removeDuplicates(int[] nums) {
+        int count = 1, i = 0, j= 0;
+        while (j < nums.length) {
+           if (j - 1 >= 0 && nums[j] == nums[j- 1]) {
+               count++;
+               if (count <= 2) {
+                   nums[i++] = nums[j++];
+               } else {
+                   j++;
+               }
+           } else {
+               count = 1;
+               nums[i++] = nums[j++];
+           }
+        }
+        return i;
+    }
+
+
+    // 209. Minimum Size Subarray Sum
+    // **** 注意边界条件，双重循环
+    public int minSubArrayLen(int s, int[] nums) {
+        if (nums == null) {
+            return 0;
+        }
+        int slow = 0, fast = 0;
+        int sum = 0;
+        int res = Integer.MAX_VALUE;
+        while (fast < nums.length) {
+            sum += nums[fast];
+            fast++;
+            while (sum >=s) {
+                res = Math.min(res, fast - slow);
+                sum -= nums[slow];
+                slow++;
+            }
+        }
+        return res == Integer.MAX_VALUE ? 0 : res;
+    }
+
+    // 24. Swap Nodes in Pairs
+    // **** 理解递归
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(head.next.next);
+        newHead.next = head;
+        return newHead;
+    }
+
+    // 迭代，辅助指针，先局部翻转，再移动指针
+    public ListNode swapPairs1(ListNode head) {
+        ListNode helper = new ListNode(-1);
+        helper.next = head;
+        ListNode newHead = helper;
+        while (head != null && head.next != null) {
+            ListNode second = head.next;
+            head.next = second.next;
+            second.next = head;
+            helper.next = second;
+            helper = head;
+            head = head.next;
+        }
+        return newHead.next;
+    }
+
+    // 143. Reorder List
+    // ***** 链表翻转、链表插入、值传递、 注意边界条件
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null || head.next.next == null) {
+            return;
+        }
+        ListNode middle = getMiddle(head);
+        ListNode first = head;
+        ListNode second = middle.next;
+        middle.next = null;
+        mergeList(first, reverse(second));
+    }
+    // 考虑奇数和偶数
+    public ListNode getMiddle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    //　迭代
+    private ListNode reverse(ListNode head) {
+        ListNode pre = head;
+        ListNode cur = head.next;
+        ListNode next = null;
+        pre.next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+    // 递归
+    private ListNode reverse1(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverse1(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+
+    private void mergeList(ListNode node1, ListNode node2) {
+        ListNode next1 = null;
+        ListNode next2 = null;
+        while (node2 != null) {
+            // 保存
+            next1 = node1.next;
+            next2 = node2.next;
+            //　拼接
+            node1.next = node2;
+            node2.next = next1;
+            // 移动指针
+            node1 = next1;
+            node2 = next2;
+        }
+    }
+
+    // 147. Insertion Sort List
+    // ****
+    public ListNode insertionSortList(ListNode head) {
+        ListNode helper = new ListNode(-1);
+        ListNode newHead = helper;
+        ListNode next = null;
+        while (head != null) {
+            // helper.next
+            while (helper.next != null && helper.next.val < head.val) {
+                helper = helper.next;
+            }
+            next = head.next;
+            // 在helper和helper.next之间插入
+            head.next = helper.next;
+            helper.next = head;
+
+            helper = newHead;
+            head = next;
+        }
+        return newHead.next;
+        
+    }
+
+    // 148. Sort List
+    // ****
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode middle = getMiddle1(head);
+        ListNode start = middle.next;
+        middle.next = null;
+        ListNode first = sortList(head);
+        ListNode second = sortList(start);
+        return merge(first, second);
+    }
+
+    public ListNode getMiddle1(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    private ListNode merge(ListNode first, ListNode second) {
+        ListNode head = new ListNode(-1);
+        ListNode helper = head;
+        while (first != null && second != null) {
+            if (first.val < second.val) {
+                head.next = first;
+                first = first.next;
+            } else {
+                head.next = second;
+                second = second.next;
+            }
+            head = head.next;
+        }
+
+        head.next = first != null ? first : second;
+        return helper.next;
+    }
+
+
+    // 2. Add Two Numbers
+    // ***
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode newHead = new ListNode(-1);
+        ListNode helper = newHead;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int value = carry;
+            if (l1 != null) {
+                value += l1.val;
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                value += l2.val;
+                l2 = l2.next;
+            }
+            carry = value / 10;
+            value = value % 10;
+            ListNode cur = new ListNode(value);
+            newHead.next = cur;
+            newHead = newHead.next;
+        }
+        if (carry != 0) {
+            newHead.next = new ListNode(carry);
+        }
+        return helper.next;
+    }
+    
 }
